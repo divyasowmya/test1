7751164bcae45f1dc4238ef9e7fb6689110f8479
peeyush.singhal <peeyush.singhal@JPDC2-L-9RXRPP3>
2024-04-18 11:33:28 +0530

    new commit for new server

--- commit1.java
+++ commit1.java
@@ -5,6 +5,6 @@ public class demo {
     System.out.println("demo1!");
     System.out.println("demo2!"); 
     System.out.println("new demo"); 
-    System.out.println("new demo1"); 
+    System.out.println("new demo1 in new server"); 
   }
 }
